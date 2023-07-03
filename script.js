950640a77c4b373afc245100d38968cc038c4d6f
MiseryFell <stanvreeland@live.com>
2023-07-03 04:04:40 -0400

    made changes to styling

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function passwordMatcher() {
     console.log("function running!")
     if (pwd.value != cfpwd.value) {
         matchText.textContent = "* Passwords do not match.";
+        pwd.setAttribute("class", "invalid-password");
+        cfpwd.setAttribute("class", "invalid-password");
     } else {
         matchText.textContent = " ";
     }
