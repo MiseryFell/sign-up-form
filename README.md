f50db6fdd7384d21030ad904340e2bf8acc7f889
MiseryFell <stanvreeland@live.com>
2023-07-03 03:25:15 -0400

    added password matching and adjusted styles

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sign-up-form
+
+## This project is mostly complete, but I haven't gotten to adding compatability to mobile, although I have intentions to do such sooner or later.
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-pwd = document.getElementById("password")
-cfpwd = document.getElementById("cf_password")
+const pwd = document.getElementById("password");
+const cfpwd = document.getElementById("cf_password");
+const matchText = document.getElementById("password-match");
 
-if (pwd != cfpwd) {
-    
+
+function passwordMatcher() {
+    console.log("function running!")
+    if (pwd.value != cfpwd.value) {
+        matchText.textContent = "* Passwords do not match.";
+    } else {
+        matchText.textContent = " ";
+    }
 }
+
+
+pwd.addEventListener("input", passwordMatcher);
+cfpwd.addEventListener("input", passwordMatcher);
